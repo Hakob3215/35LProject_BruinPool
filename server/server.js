78937f5b63d16f54e66d0eacb4002d9aeaf25dbb
Hakob3215 <hatajyan@gmail.com>
2024-02-29 14:47:26 -0800

    Add server.js template code

--- server/server.js
+++ server/server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 count = 0;
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 app.get('/', (req, res) => {
     count++;
     res.send('Hello World!, Opened '+ count + ' times');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
 
 app.listen(port, () => {
