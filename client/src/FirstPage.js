31681cf9c67c27a50487d5ec17aaab5a51837407
underlyingtech21 <123848427+underlyingtech21@users.noreply.github.com>
2024-02-29 16:01:55 -0800

    First Page
    updated the first page and added css

--- client/src/FirstPage.js
+++ client/src/FirstPage.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from './logo.svg'; 
+import './FirstPage.css';
+
 
 function FirstPage({ handleSignInClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" ></img>
-        <p>
-          Welcome to Bruin Pool
-        </p>
-        <nav>
-          <Link to="/SignInPage" className="App-link" onClick={handleSignInClick}>Sign In</Link>
-        </nav>
-      </header>
-    </div>
+
+
+<body>
+	<section class="header">
+		<nav>
+			<a> <img src="images/logo.png"/></a>
+			<div class="nav-links" id="navLinks">
+				<i class="fa fa-times" onclick="hideMenu()"></i>
+				<ul>
+					<li><a href="">HOME</a></li>
+					<li><a href="">ABOUT</a></li>
+          <li><a href="">STUDENTS</a></li>
+
+				</ul>
+
+			</div>
+
+			<i class="fas fa-bars" onclick="showMenu()"></i>
+		
+		</nav>
+
+
+<div class="text-box">
+	
+	<h1> BruinPool </h1>
+	<p>The only website to meet other carpooling college students!</p>
+	<a href="" class="hero-btn"> Sign In or Create a Free Account Today!</a>
+</div>
+	
+	</section>
+  </body>
+
+
+
   );
 }
 
