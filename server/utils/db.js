52d7be9a037acdaba18a837815e94771bd12eb12
Hakob3215 <hatajyan@gmail.com>
2024-03-06 10:30:41 -0800

    Fix mongoose connection to db.js
    Username, and email are now unique in the database, using indexes. Bug with the travel schema not showing up in the database has been fixed.

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,25 +1,20 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb+srv://bruinpool:mdjf8ElwBpfnEcYJ@bruinpoolcluster.em9znww.mongodb.net/userData?retryWrites=true&w=majority')
 .then(() => console.log('Database Connected'))
 .catch(err => console.log(err));
 
-
-const Schema = mongoose.Schema;
-
 const userSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
 
-    startTime: Number,
-    endTime: Number,
-    location: String,
-    date: Date
+    startTime: {type: Number, default: null},
+    endTime: {type: Number, default: null},
+    location: {type: String, default: null},
+    date: {type: Date, default: null}
 });
-
-const user = mongoose.model('user', userSchema);
-
 
 const travelBlogSchema = new Schema({
     title: String,
@@ -27,4 +22,18 @@ const travelBlogSchema = new Schema({
     date: Date
 });
 
+const chatLogSchema = new Schema({
+    //TBA
+});
+
+const user = mongoose.model('user', userSchema);
+
 const travelBlog = mongoose.model('travelBlog', travelBlogSchema);
+
+module.exports = {user, travelBlog};
+
+let newUser = new user({
+    username: 'test',
+    email: 'test@email.com',
+    password: 'test'
+});
